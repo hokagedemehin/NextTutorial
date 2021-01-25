92e1f253ad93a1afaf73ce3e461c11d50f7905d2
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-01-25 12:42:21 +0100

    pages and routes

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Demehin</h1>
+            <h2>Ibukun about</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae vero cum necessitatibus reprehenderit quos esse, accusamus amet molestiae sed nam non ab. Similique accusamus molestiae est consectetur quaerat suscipit id.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque iste aliquid dignissimos illum perferendis! Optio voluptatem doloremque recusandae veniam laudantium nostrum quae asperiores incidunt, ab eveniet architecto nulla magni voluptates.</p>
+        </div>
+    )
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org"> Demehin Next.js!</a>
         </h1>
+        <p>Demehin Ibukun Homepage</p>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -21,7 +22,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h3>Demehin Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
